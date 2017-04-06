db57990b7c6675e3f77018a67d655f98a520cb13
abarletta <abarletta@econ.au.dk>
2017-04-06 18:49:24 +0200

    Deleting

--- source_code/constraintinterceptols.m
+++
@@ -1,5 +0,0 @@
-function [c,ceq]=constraintinterceptols(beta,Mu,delta)
-MS=Mu;
-ceq=delta-mean(MS*beta);
-c=[];
-end
--- source_code/vec.m
+++
@@ -1,7 +0,0 @@
-%% VEC - Matrix vectorization
-% v=vec(A);
-
-function v=vec(A);
-[m n] = size(A);
-v = reshape(A,m*n,1);
-end
